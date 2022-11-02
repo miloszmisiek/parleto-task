f01ac586bbe05b89dc438a2e34dc9a45aff4d18c
miloszmisiek <84859537+miloszmisiek@users.noreply.github.com>
2022-11-02 20:01:28 +0100

    Add date filtering and category/date sorting functionality

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,12 +1,18 @@
 from django import forms
 from .models import Expense
 
+ORDERING_CHOICES = [(0, 'Ascending'), (1, 'Descending')]
+
 
 class ExpenseSearchForm(forms.ModelForm):
     class Meta:
         model = Expense
-        fields = ('name',)
+        fields = ('name', 'date',)
+        widgets = {
+            'date': forms.widgets.DateInput(attrs={'type': 'date'}),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.fields['name'].required = False
+        self.fields['date'].required = False
--- expenses/views.py
+++ expenses/views.py
@@ -11,20 +11,38 @@ class ExpenseListView(ListView):
 
     def get_context_data(self, *, object_list=None, **kwargs):
         queryset = object_list if object_list is not None else self.object_list
-
+        sort = self.request.GET.get("sort")
+        dir = self.request.GET.get("dir")
         form = ExpenseSearchForm(self.request.GET)
         if form.is_valid():
             name = form.cleaned_data.get('name', '').strip()
+            date = form.cleaned_data.get('date')
             if name:
                 queryset = queryset.filter(name__icontains=name)
+            if date:
+                queryset = queryset.filter(date=date)
+
+        if sort:
+            if sort == "date":
+                if dir == "asc":
+                    queryset = queryset.order_by('date')
+                if dir == "desc":
+                    queryset = queryset.order_by('-date')
+            if sort == "category":
+                if dir == "asc":
+                    queryset = queryset.order_by('category')
+                if dir == "desc":
+                    queryset = queryset.order_by('-category')
 
         return super().get_context_data(
             form=form,
             object_list=queryset,
             summary_per_category=summary_per_category(queryset),
+            sort=sort,
+            dir=dir,
             **kwargs)
+
 
 class CategoryListView(ListView):
     model = Category
     paginate_by = 5
-
